9b8e99a0e7db2e547479ec833431e10611593aad
amadeusdotpng <amadeus8182@gmail.com>
2025-08-24 19:22:19 -0500

    working REPL!

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,31 @@ use ctx::TypeContext;
 
 mod typck;
 
-fn main() {
-    let src = r"\f. \x. f (f x)";
-    let e = match parse::parse(src) {
-        Ok(e)  => e,
-        Err(e) => { eprintln!("{:?}", e); return; },
-    };
+use std::io;
+use std::io::Write;
 
-    // \f. \x. f (f x) : ('a -> 'a) -> 'a -> 'a
-    let mut ctx = TypeContext::new();
-    match typck::infer(&mut ctx, &e) {
-        Ok(t)  => println!("{} : {}", src, t),
-        Err(e) => eprintln!("{}", e),
+fn main() {
+    let mut buf = String::new();
+    let stdin = io::stdin();
+    loop {
+        print!(">> ");
+        let _ = io::stdout().flush();
+        if let Err(e) = stdin.read_line(&mut buf) {
+            eprintln!("{}", e); return;
+        }
+
+        let e = match parse::parse(buf.as_str()) {
+            Ok(e)  => e,
+            Err(e) => { eprintln!("{:?}", e); return; },
+        };
+
+        // \f. \x. f (f x) : ('a -> 'a) -> 'a -> 'a
+        let mut ctx = TypeContext::new();
+        match typck::infer(&mut ctx, &e) {
+            Ok(t)  => println!("{} : {}\n", buf.trim(), t),
+            Err(e) => eprintln!("{}", e),
+        }
+
+        buf.clear();
     }
 }
