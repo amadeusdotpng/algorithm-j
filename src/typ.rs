514a26082e226024767896e602b721fcc97d7489
amadeusdotpng <amadeus8182@gmail.com>
2025-08-30 12:14:10 -0500

    use characters instead of id for unbound variables

--- src/typ.rs
+++ src/typ.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::HashSet;
 
 #[derive(Debug)]
 pub struct PolyType {
@@ -24,6 +25,12 @@ pub enum MonoType {
     Var { tvar: RefCell<VarType> },
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum VarType {
+    Bound { typ: Rc<MonoType> },
+    Unbound { id: u8 },
+}
+
 impl MonoType {
     pub fn as_poly(self: Rc<Self>) -> PolyType {
         let tvar_ids = Rc::new([]);
@@ -33,7 +40,46 @@ impl MonoType {
 
 impl std::fmt::Display for MonoType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
+        let mut unbound_vars = HashSet::new();
+        find_vars(&mut unbound_vars, self);
+
+        let mut unbound_vars = unbound_vars.into_iter().collect::<Vec<_>>();
+        unbound_vars.sort();
+
+        let disp_type = DisplayType::new(self, &unbound_vars);
+        write!(f, "{disp_type}")
+    }
+}
+
+fn find_vars(unbound_vars: &mut HashSet<u8>, typ: &MonoType) {
+    match typ {
+        MonoType::Bool => {},
+        MonoType::Func { l, r } => {
+            find_vars(unbound_vars, l);
+            find_vars(unbound_vars, r);
+        }
+        MonoType::Var { tvar } => match &*tvar.borrow() {
+            VarType::Bound { typ }  => find_vars(unbound_vars, typ),
+            VarType::Unbound { id } => { unbound_vars.insert(*id); },
+        }
+    }
+}
+
+struct DisplayType<'typ> {
+    typ: &'typ MonoType,
+    // must be sorted + no duplicates
+    unbound_vars: &'typ [u8],
+}
+
+impl<'typ> DisplayType<'typ> {
+    fn new(typ: &'typ MonoType, unbound_vars: &'typ [u8]) -> DisplayType<'typ> {
+        DisplayType { typ, unbound_vars }
+    }
+}
+
+impl std::fmt::Display for DisplayType<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.typ {
             MonoType::Func { l, r } => {
                 let mut l = l.clone();
                 while let MonoType::Var { tvar } = &*l.clone()
@@ -42,29 +88,39 @@ impl std::fmt::Display for MonoType {
                     l = typ.clone();
                     continue;
                 }
+
+                let r = DisplayType::new(r, self.unbound_vars);
                 match &*l {
-                    MonoType::Func { l: l_l, r: l_r } => write!(f, "({l_l} -> {l_r}) -> {r}"),
-                    l => write!(f, "{l} -> {r}")
+                    MonoType::Func { l: l_l, r: l_r } => {
+                        let l_l = DisplayType::new(l_l, self.unbound_vars);
+                        let l_r = DisplayType::new(l_r, self.unbound_vars);
+                        write!(f, "({l_l} -> {l_r}) -> {r}")
+                    }
+                    l => {
+                        let l = DisplayType::new(l, self.unbound_vars);
+                        write!(f, "{l} -> {r}")
+                    }
                 }
             }
 
-            MonoType::Var { tvar } => write!(f, "{}", tvar.borrow()),
+            MonoType::Var { tvar } => match &*tvar.borrow() {
+                VarType::Bound { typ } => {
+                    let typ = DisplayType::new(typ, self.unbound_vars);
+                    write!(f, "{}", typ)
+                }
+
+                VarType::Unbound { id } => {
+                    let offset = self.unbound_vars.iter()
+                        .position(|n| n == id)
+                        .expect(format!("Unbound variable of id {id} not found in list").as_str());
+
+                    // Hopefully there won't be more than 26 unbound variables
+                    let char_id = ((b'a' as u8) + offset as u8) as char;
+                    write!(f, "'{char_id}")
+                }
+            }
+
             MonoType::Bool => write!(f, "Bool"),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum VarType {
-    Bound { typ: Rc<MonoType> },
-    Unbound { id: u8 },
-}
-
-impl std::fmt::Display for VarType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            VarType::Bound { typ }  => write!(f, "{}", typ),
-            VarType::Unbound { id } => write!(f, "'t{}", id),
         }
     }
 }
